ecbabf87c4d461f3641de32d3d7da1166cbb801c
Tim Warner <timothywarner316@gmail.com>
2018-03-03 16:01:30 -0600

    Add remoting support
    Made computername parameter default to localhost
    
    Added loop to iterate through the computername array

--- Get-CurrentUserContext.ps1
+++ Get-CurrentUserContext.ps1
@@ -4,8 +4,8 @@ function Get-CurrentUserContext
   [Alias('whom')]
   Param
   (
-    [Parameter(ValueFromPipeline)]
-    [string[]]$ComputerName
+    [Parameter(ValueFromPipeline, Position=1)]
+    [string[]]$ComputerName = $env:COMPUTERNAME
   )
 
   Begin
@@ -14,34 +14,37 @@ function Get-CurrentUserContext
     
   Process
   {
-    $userName = $env:USERNAME
-    $isAdmin =  ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
-    $hostName = $env:COMPUTERNAME        
-    $isDomain = (Get-CimInstance -ClassName Win32_ComputerSystem).Domain
-    $isWorkgroup = (Get-CimInstance -ClassName Win32_ComputerSystem).Workgroup
-    $FQDN=(Get-CimInstance -ClassName Win32_ComputerSystem).DNSHostName + "." + $isDomain
+      foreach ($comp in $ComputerName)
+      {
+          $userName = Invoke-Command -ComputerName $comp -ScriptBlock {$env:USERNAME}   
+          $isAdmin =  ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+          $hostName = Invoke-Command -ComputerName $comp -ScriptBlock {$env:COMPUTERNAME}        
+          $isDomain = (Get-CimInstance -ClassName Win32_ComputerSystem -ComputerName $comp).Domain
+          $isWorkgroup = (Get-CimInstance -ClassName Win32_ComputerSystem -ComputerName $comp).Workgroup
+          $FQDN= (Get-CimInstance -ClassName Win32_ComputerSystem -ComputerName $comp).DNSHostName + "." + $isDomain
        
-    $data = [ordered]@{
-      User        = $userName
-      IsAdmin     = $isAdmin
-      Host        = $hostName
-      Workgroup   = $isWorkgroup
-      Domain      = $isDomain
-      FQDN        = $FQDN
-    }
+          $data = [ordered]@{
+              User        = $userName
+              IsAdmin     = $isAdmin
+              Host        = $hostName
+              Workgroup   = $isWorkgroup
+              Domain      = $isDomain
+              FQDN        = $FQDN
+          }
 
-    $out = New-Object -TypeName PSCustomObject -Property $data
+          $out = New-Object -TypeName PSCustomObject -Property $data
+
+          if ((Get-CimInstance -ClassName Win32_ComputerSystem).PartOfDomain -eq $true)
+              {
+                  $out | Select-Object -Property User, IsAdmin, Host, Domain, FQDN
+              }
+          else
+              {
+                  $out | Select-Object -Property User, IsAdmin, Host, Workgroup, FQDN
+              }
+      }
   }
   End
   {
-    if ((Get-CimInstance -ClassName Win32_ComputerSystem).PartOfDomain -eq $true)
-    {
-      $out | Select-Object -Property User, IsAdmin, Host, Domain, FQDN
-    }
-    else
-    {
-      $out | Select-Object -Property User, IsAdmin, Host, Workgroup, FQDN
-    }
   }
 }
-Get-CurrentUserContext
